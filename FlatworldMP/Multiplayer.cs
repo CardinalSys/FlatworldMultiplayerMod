a456d33d926d4c1ba53dbcf6a1e618a2a6e9848e
Cardinal <alessandro.benvenuti09@gmail.com>
2024-04-06 18:10:03 +0200

    Added playable beta

---
+++ FlatworldMP/Multiplayer.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+
+namespace FlatworldMP
+{
+    public class Multiplayer : MelonMod
+    {
+        private GameObject playerObj;
+        private PlayerCTRL playerCtrl;
+        private CharacterController playerCharacterController;
+        private List<GameObject> playerModels = new List<GameObject>();
+
+        private GameObject secondPlayerObj;
+        private PlayerCTRL secondPlayerCtrl;
+
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (GetPlayerData() && secondPlayerObj == null)
+            {
+                SpawnSecondPlayer();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+
+
+            FirstPlayerController();
+            SecondPlayerController();
+
+
+            if (Input.GetKeyDown(KeyCode.O) && secondPlayerObj == null)
+            {
+                if (!SpawnSecondPlayer())
+                    LoggerInstance.Msg("Error spawning second player");
+
+                LoggerInstance.Msg("Second player spawned");
+            }
+
+            if(secondPlayerObj != null)
+                Traverse.Create(secondPlayerObj).Field("warps").SetValue(null);
+
+
+
+            base.OnUpdate();
+        }
+
+        private void FirstPlayerController()
+        {
+            if (Input.GetKeyDown(KeyCode.X) && playerObj != null && !Input.GetKey(KeyCode.C))
+            {
+                var codigoAtacarMethod = AccessTools.Method(typeof(PlayerCTRL), "codigoAtacar") ;
+
+                codigoAtacarMethod.Invoke(playerCtrl, null);
+            }
+
+            if (Input.GetKeyDown(KeyCode.C) && playerObj != null && !Input.GetKey(KeyCode.X))
+            {
+                var codigoMagiarMethod = AccessTools.Method(typeof(PlayerCTRL), "codigoMagiar");
+
+                codigoMagiarMethod.Invoke(playerCtrl, null);
+            }
+        }
+
+        private void SecondPlayerController()
+        {
+            if (Input.GetKeyDown(KeyCode.O) && secondPlayerObj != null && !Input.GetKey(KeyCode.P))
+            {
+                var codigoAtacarMethod = AccessTools.Method(typeof(PlayerCTRL), "codigoAtacar");
+
+                codigoAtacarMethod.Invoke(secondPlayerCtrl, null);
+            }
+
+            if (Input.GetKeyDown(KeyCode.P) && secondPlayerObj != null && !Input.GetKey(KeyCode.O))
+            {
+                var codigoMagiarMethod = AccessTools.Method(typeof(PlayerCTRL), "codigoMagiar");
+
+                codigoMagiarMethod.Invoke(secondPlayerCtrl, null);
+            }
+
+        }
+
+
+        private bool SpawnSecondPlayer() {
+            secondPlayerObj = GameObject.Instantiate(playerObj);
+            if(secondPlayerObj == null) return false;
+            secondPlayerObj.name = "SecondPlayer";
+            secondPlayerCtrl = secondPlayerObj.GetComponent<PlayerCTRL>();
+
+            //Hardcode walkSpeed to match runSpeed.
+            Traverse.Create(secondPlayerCtrl).Field("walkSpeed").SetValue(5);
+            return true;
+        }
+
+
+        private bool GetPlayerData()
+        {
+            playerObj = GameObject.Find("obj_player");
+            if (playerObj == null)
+                return false;
+            playerCtrl = playerObj.GetComponent<PlayerCTRL>();
+
+            playerCharacterController = playerObj.GetComponent<CharacterController>();
+            UpdatePlayerModels();
+
+            return true;
+        }
+
+        private bool UpdatePlayerModels()
+        {
+            GameObject CHAR = GameObject.Find("CHAR");
+            if (CHAR == null) return false;
+            playerModels.AddRange(CHAR.GetComponentsInChildren<GameObject>());
+
+            if (playerModels.Count > 0) return true;
+            else return false;
+        }
+    }
+
+
+
+
+    [HarmonyLib.HarmonyPatch(typeof(PlayerCTRL), "getInputKeyboard")]
+    static class InputPatch
+    {
+
+        static bool Prefix(PlayerCTRL __instance, ref float[] __result)
+        {
+
+            float[] array = new float[2];
+
+            if (__instance.gameObject.name == "SecondPlayer")
+            {
+                if (Input.GetKey(KeyCode.K))
+                {
+                    array[1] = -1f;
+                }
+                if (Input.GetKey(KeyCode.I))
+                {
+                    array[1] = 1f;
+                }
+                if (Input.GetKey(KeyCode.J))
+                {
+                    array[0] = -1f;
+                }
+                if (Input.GetKey(KeyCode.L))
+                {
+                    array[0] = 1f;
+                }
+
+
+                __result = array;
+
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+
+    [HarmonyLib.HarmonyPatch(typeof(MotherBrain), "get_pulsa_ATACAR_down")]
+    static class AttackPatch
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(MotherBrain), "get_pulsa_MAGIA_down")]
+    static class MagicPatch
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(PlayerCTRL), "OnTriggerEnter")]
+    static class TriggerEnterPatch
+    {
+
+        static bool Prefix(Collider other, PlayerCTRL __instance)
+        {
+
+            if (__instance.gameObject.name == "SecondPlayer")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(PlayerCTRL), "OnTriggerEnter")]
+    static class TriggerExitPatch
+    {
+
+        static bool Prefix(Collider other, PlayerCTRL __instance)
+        {
+
+            if (__instance.gameObject.name == "SecondPlayer")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(PlayerCTRL), "OnTriggerEnter")]
+    static class TriggerStayPatch
+    {
+
+        static bool Prefix(Collider other, PlayerCTRL __instance)
+        {
+
+            if (__instance.gameObject.name == "SecondPlayer")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+
+
+}
---
+++ FlatworldMP/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using FlatworldMP;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: MelonInfo(typeof(Multiplayer), "FlatworldMP", "0.1", "Sora")]
+[assembly: MelonGame("Guinxu", "Flatworld")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FlatworldMP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlatworldMP")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55bb2eef-9148-45a5-87c0-9f90dbbdcd0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
